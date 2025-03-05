cbb4bb9870e279e5c551745b9b8b3feea1c08183
Aleksandr2905 <natalukha@ukr.net>
2025-03-05 09:03:39 +0200

    added sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import { Audience } from '@/sections/audience';
 import { Career } from '@/sections/career';
+import { Cases } from '@/sections/cases';
 import { Community } from '@/sections/community';
 import { Contact } from '@/sections/contact';
+import { Education } from '@/sections/education';
 import { Hero } from '@/sections/hero';
+import { Programme } from '@/sections/programme';
+import { Reviews } from '@/sections/reviews';
+import { Speakers } from '@/sections/speakers';
+import { Tariffs } from '@/sections/tariffs';
 
 export default function Home() {
   return (
@@ -10,14 +16,14 @@ export default function Home() {
       <main>
         <Hero />
         <Audience />
+        <Speakers />
+        <Education />
+        <Cases />
         <Career />
+        <Programme />
         <Community />
-        <div id="portfolio" className="h-[500px]">
-          Portfolio
-        </div>
-        <div id="pages" className="h-[500px]">
-          Pages
-        </div>
+        <Tariffs />
+        <Reviews />
         <Contact />
       </main>
     </>
---
+++ src/sections/education/education.tsx
@@ -0,0 +1,7 @@
+export const Education = () => {
+  return (
+    <section className="container py-10 xl:py-[100px] flex flex-col gap-7 xl:gap-10">
+      <h2>Як проходить навчання</h2>
+    </section>
+  );
+};
