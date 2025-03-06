4b1bb0841c70b3b6088757317d717fc7a1b99055
Aleksandr2905 <natalukha@ukr.net>
2025-03-06 09:26:03 +0200

    added section education

--- src/sections/education/education.tsx
+++ src/sections/education/education.tsx
@@ -1,7 +1,10 @@
+import { EducationSlider } from '@/components/ui/education-slider';
+
 export const Education = () => {
   return (
     <section className="container py-10 xl:py-[100px] flex flex-col gap-7 xl:gap-10">
       <h2>Як проходить навчання</h2>
+      <EducationSlider />
     </section>
   );
 };
